9c979d62d2b7094642f3788adab7b0bc2694bc71
Daniel <keendaniel01@gmail.com>
2017-03-21 14:39:13 +0000

    Completed gulp build task

--- app/temp/scripts/modernizr.js
+++ app/temp/scripts/modernizr.js
@@ -1,10 +1,6 @@
 /*!
  * modernizr v3.4.0
-<<<<<<< HEAD
  * Build https://modernizr.com/download?-flexbox-svg-setclasses-dontmin
-=======
- * Build https://modernizr.com/download?-cssanimations-flexbox-svg-setclasses-dontmin
->>>>>>> browser-support
  *
  * Copyright (c)
  *  Faruk Ates
@@ -800,29 +796,6 @@
   
 /*!
 {
-<<<<<<< HEAD
-=======
-  "name": "CSS Animations",
-  "property": "cssanimations",
-  "caniuse": "css-animation",
-  "polyfills": ["transformie", "csssandpaper"],
-  "tags": ["css"],
-  "warnings": ["Android < 4 will pass this test, but can only animate a single property at a time"],
-  "notes": [{
-    "name" : "Article: 'Dispelling the Android CSS animation myths'",
-    "href": "https://goo.gl/OGw5Gm"
-  }]
-}
-!*/
-/* DOC
-Detects whether or not elements can be animated using CSS
-*/
-
-  Modernizr.addTest('cssanimations', testAllProps('animationName', 'a', true));
-
-/*!
-{
->>>>>>> browser-support
   "name": "Flexbox",
   "property": "flexbox",
   "caniuse": "flexbox",
